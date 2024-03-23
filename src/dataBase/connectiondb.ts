aacc8269b87e0d465ea66c56a65902279afaddb3
jhonatan isaias dos santos <jhonatan.santos13@academico.ifs.edu.br>
2024-03-23 13:18:09 -0300

    conectado ao cluster

--- src/dataBase/connectiondb.ts
+++ src/dataBase/connectiondb.ts
@@ -3,13 +3,19 @@ import { MongoClient } from 'mongodb';
 const uri = process.env.URL_MONGO || '';
 export const dbName = process.env.BDNAME;
 
-export const client = new MongoClient(uri);
+export const client = new MongoClient(uri, {
+  serverApi: {
+    version: '1',
+    strict: true,
+    deprecationErrors: true,
+  },
+});
 
 export const connectMongo = async () => {
   try {
     await client.connect();
     console.log('conectado ao mongo db');
-    return client.db(dbName);
+    return client.db(dbName).command({ ping: 1 });
   } catch (error) {
     console.log('error ao conectar ao mongo db', error);
   }
